30321a88fc8979e68530c486321a2c76c40e30d7
Mikhail Volkhov <volhovm.cs@gmail.com>
2017-01-16 02:35:15 +0300

    Add indices

--- project/constraints.sql
+++ project/constraints.sql
@@ -22,7 +22,23 @@ ALTER TABLE SnapshotVersions ADD CONSTRAINT FK_SVVersion_VersionId
   FOREIGN KEY (SVVersion) REFERENCES Version(VersionId);
 ALTER TABLE Build ADD CONSTRAINT FK_BuildVersion_VersionId
   FOREIGN KEY (BuildVersion) REFERENCES Version(VersionId);
+ALTER TABLE Build ADD CONSTRAINT BuildStatus_Matches_Nulls CHECK
+  ((BuildStatus = 'PENDING' AND TimeFinished IS NULL) OR
+   (BuildStatus = 'IN PROGRESS' AND TimeFinished IS NULL AND WorkDirectory IS NOT NULL) OR
+   ((BuildStatus = 'SUCCESS' OR BuildStatus = 'FAIL') AND TimeFinished IS NOT NULL AND WorkDirectory IS NOT NULL));
 ALTER TABLE Downloads ADD CONSTRAINT FK_DVersion_VersionId
   FOREIGN KEY (DVersion) REFERENCES Version(VersionId);
 ALTER TABLE Downloads ADD CONSTRAINT FK_DBinaryBuild_BuildId
   FOREIGN KEY (DBinaryBuild) REFERENCES Build(BuildId);
+
+---- For searching packages by name/regex.
+CREATE INDEX ON Package USING btree (PName);
+---- For quickly finding the package related to VersionId
+CREATE INDEX ON Version USING btree (VersionId, VPackage);
+---- For getting all versions of the package
+CREATE INDEX ON Version USING hash (VPackage);
+---- Doesn't optimize anything if there's not much users
+-- CREATE INDEX ON RegisteredUser USING hash (Name);
+---- For stats
+CREATE INDEX ON Downloads USING hash (DVersion);
+CREATE INDEX ON Downloads USING hash (DBinaryBuild);
